d8c0a4a1cf7897eff37924b7b38ee73f3a0d25e3
nicole <mailtonicoles@yahoo.com>
2018-12-15 17:17:38 +1100

    Weather API has added.

--- weather.js
+++ weather.js
@@ -1,22 +1,39 @@
-const API_KEYS = "d82780e9998bdb2dfb69cfa04284121d";
-const COORD = 'coord';
+const API_KEY = "d82780e9998bdb2dfb69cfa04284121d";
+const COORD = "coord";
+
+function getWeather(lat, lon)
+{
+    fetch
+    (
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    )
+    .then(function(response)
+    {
+        return response.json();
+    })
+    .then(function(json)
+    {
+        console.log(json);
+    });
+}
 
 function saveCoord(coordObj)
 {
     localStorage.setItem(COORD, JSON.stringify(coordObj));
 }
 
-function handleGeoSuccess(position)
+function handleGeoSuccess(position) 
 {
     const latitude = position.coord.latitude;
     const longitude = position.coord.longitude;
     const coordObj = {latitude, longitude};
     saveCoord(coordObj);
+    getWeather(latitude, longitude);
 }
 
 function handleGeoError()
 {
-
+    console.log("Unable to access geo location.")
 }
 
 function askForCoord()
@@ -27,13 +44,14 @@ function askForCoord()
 function loadCoord()
 {
     const loadedCoord = localStorage.getItem(COORD);
-    if (loadedCoord === null)
+    if (loadedCoord === null) // no coord is in the local storage
     {
         askForCoord();
     }
-    else
+    else // when already have the coord
     {
-        //get weather
+        const parsedCoord = JSON.parse(loadedCoord); //get weather
+        getWeather(parsedCoord.latitude, parsedCoord.longitude);
     }
 }
 
